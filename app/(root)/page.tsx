34105f58cae3afa58ff053c22161fd40855af4e2
Aseem5047 <aseemgupta2002@gmail.com>
2024-05-09 11:00:13 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main>
+			<Button variant="destructive" className="px-4 py-2 rounded-lg">
+				Nice
+			</Button>
+		</main>
+	);
+}
